b7763e2af52d8f35a7d25ed1b39b929eb5db8ba4
Yvonne Kanvu <yvkanvu@gmail.com>
2023-12-14 12:45:02 +0100

    create section

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { appRoutes } from '../../lib/routes'
+import InputForm from '../Form/InputForm'
 
 export default function HomePage() {
   return (
-    <section>
+    <section className='flex flex-row'>
       <div className=' bg-violet-700 w-[33%] h-screen'></div>
-      <div className=' bg-white'></div>
+      <div className=' bg-white'><p className=' text-black'>Formulaire</p>
+      <InputForm/>
+      </div>
     </section>
   )
 }
